65a058c7fb61a0341f1fe6fffffc273d384b2f99
Matthew Weier O'Phinney <matthew@zend.com>
2017-07-31 12:21:57 -0500

    Removed unnecessary `RuntimeException` class
    There's no need for an extra level of abstraction here; just extend the
    SPL `RuntimeException` and implement the package-level interface.

--- src/Exception/InvalidConfigException.php
+++ src/Exception/InvalidConfigException.php
@@ -7,6 +7,8 @@
 
 namespace Zend\Expressive\Cache\Exception;
 
-class InvalidConfigException extends RuntimeException
+use RuntimeException;
+
+class InvalidConfigException extends RuntimeException implements ExceptionInterface
 {
 }
